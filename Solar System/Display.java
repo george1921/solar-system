dfca41f91a143d7ceff204c4e56fba642334a3df
jianwei.gan <jianwei.gan@d0cba1ac-a3f0-11de-bea2-1db044c32224>
2009-09-20 16:32:28 +0000

    Camera movement works. Use up/down/left/right to rotate camera and I and O to zoom in/out.

--- Solar System/Display.java
+++ Solar System/Display.java
@@ -9,6 +9,8 @@ import planets.SolarSystem;
 import planets.Sun;
 
 import com.sun.opengl.util.gl2.GLUT;
+
+import java.awt.event.KeyEvent;
 import java.util.*;
 
 
@@ -19,8 +21,15 @@ import java.util.*;
  */
 public class Display extends Scene
 {
-    // animation state
-    private float myAngle = 0.0f;
+	// constants
+    public static final Point3 DEFAULT_CAMERA_FROM = new Point3(65, 13, 3);
+    public static final Point3 DEFAULT_CAMERA_TO = new Point3(0, 0, 0);
+    public static final Point3 DEFAULT_CAMERA_UP = new Point3(0, 1, 0);
+    
+	// animation state
+    private Point3 myCamFrom;
+    private Point3 myCamTo;
+    private Point3 myCamUp;
 	SolarSystem ss = new SolarSystem();
 
     /**
@@ -32,7 +41,7 @@ public class Display extends Scene
      */
     public Display (String[] args)
     {
-        // do nothing
+        setDefaultCamera();
     }
 
 
@@ -60,9 +69,9 @@ public class Display extends Scene
     public void setCamera (GL2 gl, GLU glu, GLUT glut)
     {
         glu.gluLookAt(
-	        65,  13,  3,            // from position
-		    0,  0,  0,            // to position
-		    0,  1,  0);           // up direction
+	        myCamFrom.x,  myCamFrom.y,  myCamFrom.z,            // from position
+	        myCamTo.x,  myCamTo.y,  myCamTo.z,            // to position
+	        myCamUp.x,  myCamUp.y,  myCamUp.z);           // up direction
     }
 
 
@@ -77,4 +86,92 @@ public class Display extends Scene
         // animate model by spinning it a few degrees each time
 //        myAngle += 4;
     }
+    
+
+    /**
+     * Respond to the press of a key.
+     * 
+     * @param keyCode Java code representing pressed key
+     */
+    public void keyPressed (int keyCode)
+    {
+    	Vector3 viewDir = getViewDir();
+    	Vector3 upDir = new Vector3(myCamUp);
+    	upDir.normalize();
+    	
+    	Vector3 rightDir = new Vector3();
+    	rightDir.cross(viewDir, upDir);
+    	rightDir.normalize();
+    	
+        switch (keyCode) {
+		    case KeyEvent.VK_I: // Zoom In
+		    	myCamFrom.sub(viewDir);
+		    	break;
+		    case KeyEvent.VK_O: // Zoom out;
+		    	myCamFrom.add(viewDir);
+		    	break;
+		    // Camera movement: Current implementation rotates around the spot.
+            case KeyEvent.VK_LEFT:
+            	myCamTo.sub(rightDir);
+            	break;
+            case KeyEvent.VK_RIGHT:
+            	myCamTo.add(rightDir);
+            	break;
+            case KeyEvent.VK_UP:
+            	myCamTo.add(upDir);
+            	break;
+            case KeyEvent.VK_DOWN:
+            	myCamTo.sub(upDir);
+            	break;
+        }
+    }
+
+
+    /**
+     * Respond to the release of a key.
+     * 
+     * @param keyCode Java code representing released key
+     */
+    public void keyReleased (int keyCode)
+    {
+    	
+        switch (keyCode)
+        {
+            // toggle animation running
+            case KeyEvent.VK_H:
+                // hide orbits
+                break;
+            case KeyEvent.VK_R:
+            	// restart simulation
+            	break;
+            // Reset camera view
+            case KeyEvent.VK_C:
+            	setDefaultCamera();
+            	break;
+        }
+    }
+
+
+    /**
+     * Respond to the press and release of an alphanumeric key.
+     * 
+     * @param key text representing typed key
+     */
+    public void keyTyped (int keyCode)
+    {
+        // by default, do nothing
+    }
+    
+    private Vector3 getViewDir() {
+    	Vector3 viewDir = new Vector3();
+    	viewDir.sub(myCamFrom, myCamTo);
+    	viewDir.normalize();
+    	return viewDir;
+    }
+    
+	private void setDefaultCamera() {
+		myCamFrom = DEFAULT_CAMERA_FROM;
+        myCamTo = DEFAULT_CAMERA_TO;
+        myCamUp = DEFAULT_CAMERA_UP;
+	}
 }
